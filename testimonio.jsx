c2c94dca443e3376dcf2a823da9f0e885f27dff5
EstefaniaReGo26 <161658840+EstefaniaReGo26@users.noreply.github.com>
2024-03-27 21:21:27 -0500

    Add files via upload

---
+++ testimonio.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import '../stylesheets/testimonio.css';
+
+function testimonio(props) {
+  return(
+    <div className="contenedor-testimonio" >
+      <img className="imagen-testimonio" 
+      src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+      alt={props.persona} />
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio"><strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className="cargo-testimonio">{props.cargo} at <strong>{props.empresa}</strong></p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
+      </div>
+    </div>
+  );
+}
+
+export default testimonio;
